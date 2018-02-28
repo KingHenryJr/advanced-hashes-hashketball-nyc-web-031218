512e74dd4ea20058206fca50896397e60ca8dcfa
Henry Zhang <henryyihengzhang@gmail.com>
2018-02-28 20:33:47 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,20 +3,68 @@
 def game_hash
   game = {
     :home => {
-      :team_name => "Team Name"
-      :colors => []
+      :team_name => "Brooklyn Nets"
+      :colors => ["Black". "White"]
       :players => {
-        "player1" => {
-          :number => "5"
-          :shoe =>
-          :points =>
-          :rebounds =>
-          :assists =>
-          :steals =>
-          :blocks =>
-          :slam_dunks =>
+        "Alan Anderson" => {
+          :number => "0"
+          :shoe => "16"
+          :points => "22"
+          :rebounds => "12"
+          :assists => "12"
+          :steals => "3"
+          :blocks => "1"
+          :slam_dunks => "1"
         }
-        
+        "Reggie Evans" => {
+          :number => "30"
+          :shoe => "14"
+          :points => "12"
+          :rebounds => "12"
+          :assists => "12"
+          :steals => "12"
+          :blocks => "12"
+          :slam_dunks => "7"
+        }
+        "Alan Anderson" => {
+          :number => "0"
+          :shoe => "16"
+          :points => "22"
+          :rebounds => "12"
+          :assists => "12"
+          :steals => "3"
+          :blocks => "1"
+          :slam_dunks => "1"
+        }
+        "Brook Lopez" => {
+          :number => "11"
+          :shoe => "17"
+          :points => "17"
+          :rebounds => "19"
+          :assists => "10"
+          :steals => "3"
+          :blocks => "1"
+          :slam_dunks => "15"
+        }
+        "Mason Plumlee" => {
+          :number => "1"
+          :shoe => "19"
+          :points => "26"
+          :rebounds => "12"
+          :assists => "6"
+          :steals => "3"
+          :blocks => "8"
+          :slam_dunks => "5" 
+        }  
+        "Jason Terry" => {
+          :number => "31"
+          :shoe => "15"
+          :points => "19"
+          :rebounds => "2"
+          :assists => "2"
+          :steals => "4"
+          :blocks => "11"
+          :slam_dunks => "1"  
       }
     }
     
