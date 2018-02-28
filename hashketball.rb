44a971c9d41169a71fe6ce7f0ab752ee5d2a914e
Henry Zhang <henryyihengzhang@gmail.com>
2018-02-28 21:02:10 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -83,14 +83,14 @@ def game_hash
           slam_dunks: "2"
           },
         "DeSagna Diop" => {
-          :number => "2",
-          :shoe => "14",
-          :points => "24",
-          :rebounds => "12",
-          :assists => "12",
-          :steals => "4",
-          :blocks => "5",
-          :slam_dunks => "5",
+          number: "2",
+          shoe: "14",
+          points: "24",
+          rebounds: "12",
+          assists: "12",
+          steals: "4",
+          blocks: "5",
+          slam_dunks: "5",
           },
         "Ben Gordon" => {
           :number => "8",
