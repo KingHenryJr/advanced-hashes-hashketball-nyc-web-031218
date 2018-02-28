c01a216a69594434572224af761cbeccd408b374
Henry Zhang <henryyihengzhang@gmail.com>
2018-02-28 21:32:23 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -118,7 +118,7 @@ end
 def num_points_scored(player_name)
   game_hash.each do |location, team_data|
     
-      if team_data[:players] == player_name
+      if team_data[:players].keys.include?(player_name)
         
         
         return data
