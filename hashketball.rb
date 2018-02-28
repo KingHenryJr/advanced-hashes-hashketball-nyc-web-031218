ea176b4a3a38bc75bb229b2de05a4f57130f5a16
Henry Zhang <henryyihengzhang@gmail.com>
2018-02-28 21:11:56 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -59,7 +59,7 @@ def game_hash
       }
     },
     :away => {
-      team_name: "Charlotte Hornets"
+      team_name: "Charlotte Hornets",
       colors: ["Turquoise", "Purple"],
       players: {
         "Jeff Adrien" => {
