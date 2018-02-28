5dc8a39259d71f92dac6c9fa09c20e849b7f5099
Henry Zhang <henryyihengzhang@gmail.com>
2018-02-28 20:57:35 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -9,12 +9,12 @@ def game_hash
         "Alan Anderson" => {
           number: "0",
           shoe: "16",
-          :points => "22",
-          :rebounds => "12",
-          :assists => "12",
-          :steals => "3",
-          :blocks => "1",
-          :slam_dunks => "1",
+          points: "22",
+          rebounds: "12",
+          assists: "12",
+          steals: "3",
+          blocks: "1",
+          slam_dunks: "1",
           },
         "Reggie Evans" => {
           :number => "30",
