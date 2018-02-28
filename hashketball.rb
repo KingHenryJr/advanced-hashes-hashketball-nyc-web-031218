e3ffae22e8c2f7ab252999ba847e9038d471a0ab
Henry Zhang <henryyihengzhang@gmail.com>
2018-02-28 20:24:47 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,9 +3,12 @@
 def game_hash
   game = {
     :home => {
-      :team_name 
-      :colors
-      :players
+      :team_name => "Team Name"
+      :colors => []
+      :players => {
+        "player1" => 
+        
+      }
     }
     
     :away => {
